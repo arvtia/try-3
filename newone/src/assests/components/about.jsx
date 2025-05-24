c47dd8bfa3ce05d5a0b964f503f3920bffa2a4e9
Arvtia <116747258+arvtia@users.noreply.github.com>
2025-05-24 17:02:13 +0530

    message from CEO

---
+++ newone/src/assests/components/about.jsx
@@ -0,0 +1,26 @@
+
+
+export function AboutUs () {
+    return (
+        <>
+           <section className="hero is-small my-2 is-bold">
+                <div className="hero-body">
+                    <div className="column vcentered">
+                        <div className="column is-one-third">
+                            <div className="is-bold mb-2">
+                                <figure>
+                                    <img src="https://img.etimg.com/thumb/width-1200,height-900,imgsize-226741,resizemode-75,msid-64333030/magazines/panache/indra-nooyi-highest-paid-female-ceo-fewer-women-bosses-worldwide-but-their-pay-higher-than-men.jpg" alt="ceo image" className="image is-64x64 is-rounded" />
+                                </figure>
+                            </div>
+                            <p className="is-size-6 is-family-code">
+                                At Duck Company, our mission is clear: to safeguard ducks and preserve their habitats for generations to come. These beautiful and vital creatures are facing increasing threats from habitat destruction, pollution, and climate change. It is our responsibility to ensure that ducks continue to thrive in their natural environments.
+                                Thanks to your generosity, we have made great strides in conservation efforts, from restoring wetlands to advocating for stronger wildlife protections. But there is still more to do. We invite you to join us in this cause—whether by volunteering, donating, or spreading awareness.
+
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>             
+       </>
+    )
+}
