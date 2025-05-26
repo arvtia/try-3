2a7ba7f2df03556262866c99e398f4e23f50a0d4
Arvtia <116747258+arvtia@users.noreply.github.com>
2025-05-26 16:54:16 +0530

    final touch to the message and form block
    form1.jsx is done for now,
    
    next - moving towards

--- newone/src/assests/components/form1.jsx
+++ newone/src/assests/components/form1.jsx
@@ -1,41 +1,59 @@
 const Form1 = () =>{
     return (
-        <div className="column is-third-quarter ">
-            <div className="column is-half">
-                <article className="mt-3 mx-auto message">
-                    <div className="message-header">
-                        <p>Please fill the below form to become a volunteer of Duck</p>
-                        <button className="delete" aria-label="delete"></button>
-                    </div>
-                    <div className="message-body">
-                        Lorem ipsum dolor sit amet.
-                        Totam nemo corporis dolores rerum.
-                        Quaerat quidem non vel magni?
-                    </div>
+        <div className="columns  is-centered mx-2">
+            <div className="column is-full-mobile is-half-tablet">
+                <article className="message mt-3">
+                <div className="message-header">
+                    <p>Please fill out the form to become a volunteer for Duck</p>
+                    <button className="delete" aria-label="delete"></button>
+                </div>
+                <div className="message-body">
+                    Lorem ipsum dolor sit amet. Totam nemo corporis dolores rerum.
+                    Quaerat quidem non vel magni?
+                </div>
                 </article>
             </div>
 
-            <div className="column">
-                <div className="field is-horizontal">
-                    
+            <div className="column is-full-mobile is-one-third-tablet">
+                <div className="field">
+                <div className="field-body">
+                    <div className="field">
+                    <p className="control has-icons-left">
+                        <input className="input" type="text" placeholder="Name" />
+                        <span className="icon is-small is-left">
+                        <i className="bi bi-person" />
+                        </span>
+                    </p>
+                    </div>
+                    <div className="field">
+                    <p className="control has-icons-left has-icons-right">
+                        <input className="input is-success" type="email" placeholder="Email" />
+                        <span className="icon is-small is-left">
+                        <i className="bi bi-envelope" />
+                        </span>
+                        <span className="icon is-small is-right">
+                        <i className="bi bi-check" />
+                        </span>
+                    </p>
+                    </div>
+                </div>
+                </div>
+
+                <div className="field">
                     <div className="field-body">
                         <div className="field">
-                        <p className="control is-expanded has-icons-left">
-                            <input className="input" type="text" placeholder="Name" />
+                        <p className="control has-icons-left">
+                            <input className="input" type="date" />
                             <span className="icon is-small is-left">
-                            <i className="bi bi-person" />
+                            <i className="bi bi-calendar-event"></i>
                             </span>
                         </p>
                         </div>
                         <div className="field">
-                        <p className="control is-expanded has-icons-left has-icons-right">
-                            <input
-                            className="input is-success"
-                            type="email"
-                            placeholder="Email"
-                            />
+                        <p className="control has-icons-left has-icons-right">
+                            <input className="input is-success" type="number" placeholder="Enter your Phone number" />
                             <span className="icon is-small is-left">
-                            <i className="bi bi-envelope" />
+                            <i className="bi bi-telephone" />
                             </span>
                             <span className="icon is-small is-right">
                             <i className="bi bi-check" />
@@ -44,62 +62,32 @@ const Form1 = () =>{
                         </div>
                     </div>
                 </div>
-                <div className="field is-horizontal">
-                    
-                    <div className="field-body">
-                        <div className="field">
-                        <p className="control is-expanded has-icons-left">
-                            <input className="input" type="date" />
+
+                    <div className="field is-grouped">
+                        <div className="control has-icons-left">
                             <span className="icon is-small is-left">
-                                <i class="bi bi-calendar-event"></i>
+                                <i className="bi bi-geo-alt-fill" />
                             </span>
-                        </p>
-                        </div>
-                        <div className="field">
-                        <p className="control is-expanded has-icons-left has-icons-right">
-                            <input
-                            className="input is-success"
-                            type="number"
-                            placeholder="Enter your Phone number"
-                          
-                            />
-                            <span className="icon is-small is-left">
-                            <i className="bi bi-telephone" />
-                            </span>
-                            <span className="icon is-small is-right">
-                            <i className="bi bi-check"/>
-                            </span>
-                        </p>
+                            <div className="select">
+                                <select name="namecoutry" id="countryName">
+                                    <option value="Ind">New Jersey</option>
+                                    <option value="Ind">Budapest</option>
+                                    <option value="Ind">Mexico-Latvia</option>
+                                    <option value="Ind">New York</option>
+                                    <option value="Ind">London</option>
+                                </select>
+                            </div>
                         </div>
                     </div>
-                </div>
-               
-            <div className="field is-grouped">
-                <div className="control">
-                    <div className="control has-icons-left">
-                    <span className="icon is-small is-left">
-                        <i className="bi bi-geo-alt-fill" />
-                    </span>
-                        <div className="select">
-                            <select name="namecoutry" id="countryName">
-                                <option value="Ind">New Jersey</option>
-                                <option value="Ind">Budapest</option>
-                                <option value="Ind">Mexico-Latvia</option>
-                                <option value="Ind">New York</option>
-                                <option value="Ind">London</option>
-                            </select>
+                    <div className="field">
+                        <div className="control">
+                            <button className="button is-hoverable" type="submit">
+                                <i className="bi bi-send-fill"></i> Submit
+                            </button>
                         </div>
-                    </div>
-                    
-                </div>
-                    <div className="control">
-                        <button className="button  is-hoverable" type="submit">
-                        <i className="bi bi-send-fill"></i> Submit
-                        </button>
                     </div>
                 </div>
             </div>
-        </div>
 
     )
 }
